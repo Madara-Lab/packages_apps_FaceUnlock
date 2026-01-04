485e39928fb7bbcea00fcc206ba0fc43cfa73333
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-01-05 06:49:12 +0800

    moving and shipping jni libs to avoid unsatisfied link errors
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- common.mk
+++ common.mk
@@ -4,14 +4,23 @@ ifeq ($(TARGET_SUPPORTS_GFU),true)
 $(call inherit-product-if-exists, vendor/google/faceunlock/config.mk)
 else
 PRODUCT_PACKAGES += \
-    FaceUnlock
+    FaceUnlock \
+    libtensorflowlite_jni \
+    libimage_processing_util_jni \
+    libsurface_util_jni
 
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.face.sense_service=true
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.hardware.biometrics.face.xml
     
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-	system/lib64/libtensorflowlite_jni.so
+    system/lib/libimage_processing_util_jni.so \
+    system/lib/libsurface_util_jni.so \
+    system/lib/libtensorflowlite_jni.so \
+	system/lib64/libtensorflowlite_jni.so \
+	system/lib64/libimage_processing_util_jni.so \
+	system/lib64/libsurface_util_jni.so
 endif
 endif
